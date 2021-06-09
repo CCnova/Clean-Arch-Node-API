97db21e7c9c98ce33e4f6316082413d65d0ec999
CCnova <caiocnova1@gmail.com>
2021-06-09 10:12:43 -0300

    Add test case to ensure login router returns 400 if no password is provided

--- src/presentation/routers/login-router.spec.ts
+++ src/presentation/routers/login-router.spec.ts
@@ -1,6 +1,6 @@
 class LoginRouter {
     public route (httpRequest: any) {
-        if (!httpRequest.body.email) {
+        if (!httpRequest.body.email || !httpRequest.password) {
             return {
                 statusCode: 400
             }
@@ -14,6 +14,17 @@ describe('Login Router', () => {
         const httpRequest = {
             body: {
                 password: 'any_password'
+            }
+        };
+        const httpResponse = sut.route(httpRequest);
+        expect(httpResponse?.statusCode).toBe(400);
+    });
+
+    test('Should return 400 if no password is provided', () => {
+        const sut = new LoginRouter();
+        const httpRequest = {
+            body: {
+                email: 'any_email@email.com'
             }
         };
         const httpResponse = sut.route(httpRequest);
