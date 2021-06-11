f11bb88aaaba975df50517ccfbabddeab3460df6
CCnova <caiocnova1@gmail.com>
2021-06-11 16:21:19 -0300

    Add test to ensure LoadUserByEmailRepository is called with correct email

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,9 +1,15 @@
 const { MissingParamError } = require('../../utils/errors');
 
 class AuthUseCase {
+    constructor (loadUserByEmailRepository) {
+        this.loadUserByEmailRepository = loadUserByEmailRepository;
+    }
+
     async auth (email, password) {
-        if (!email) throw new MissingParamError();
-        if (!password) throw new MissingParamError();
+        if (!email) throw new MissingParamError('email');
+        if (!password) throw new MissingParamError('password');
+
+        await this.loadUserByEmailRepository.load(email);
     }
 }
 
@@ -16,7 +22,19 @@ describe('Auth UseCase', () => {
 
     test('Should throw if no password is provided', async () => {
         const sut = new AuthUseCase();
-        const authPromise = sut.auth();
+        const authPromise = sut.auth('any_email@mail.com');
         expect(authPromise).rejects.toThrow(new MissingParamError('password'));
+    });
+
+    test('Should call LoadUserByEmailRepository with correct email', async () => {
+        class LoadUserByEmailRepositorySpy {
+            async load(email) {
+                this.email = email;
+            }
+        }
+        const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy();
+        const sut = new AuthUseCase(loadUserByEmailRepositorySpy);
+        sut.auth('any_email@email.com', 'any_password');
+        expect(loadUserByEmailRepositorySpy.email).toBe('any_email@email.com');
     });
 });
