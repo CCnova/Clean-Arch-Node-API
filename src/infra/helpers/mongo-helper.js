dc4295754ef5388c48c9205c335041ede9fd0b00
CCnova <caiocnova1@gmail.com>
2021-06-14 08:56:15 -0300

    Add test to ensure MongoHelper reconnect when getDb is invoked and client is disconnected

--- src/infra/helpers/mongo-helper.js
+++ src/infra/helpers/mongo-helper.js
@@ -1,24 +1,27 @@
 const { MongoClient } = require("mongodb");
 
 module.exports = {
-    async connect (uri, dbName) {
-        this.uri = uri;
-        this.dbName = dbName;
-        this.connection = await MongoClient.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-          });
-          this.db = await this.connection.db(dbName)
-    },
+  async connect(uri, dbName) {
+    this.uri = uri;
+    this.dbName = dbName;
+    this.connection = await MongoClient.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    this.db = this.connection.db();
+  },
 
-    async disconnect () {
-        await this.connection.close();
-    },
+  async disconnect() {
+    await this.connection.close();
+    this.connection = null;
+    this.db = null;
+  },
 
-    async getDb () {
-        if (!this.connection.isConnected) await this.connect(this.uri, this.dbName);
-
-        return this.db;
+  async getDb() {
+    if (!this.connection || !this.connection.isConnected()) {
+        await this.connect(this.uri, this.dbName);
     }
-};
 
+    return this.db;
+  },
+};
